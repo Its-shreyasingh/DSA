0b24e9b37cbc8c1b81010760f47cb573fbe170f1
Its-shreyasingh <ssshreya1210@gmail.com>
2025-09-18 22:53:58 +0530

    basic-concept

--- linklst.java
+++ linklst.java
@@ -1,3 +1,23 @@
+class Node
+{
+    int data;
+    Node next;
+
+    public Node(int data1,Node next1) {
+        this.data=data1;
+        this.next=next1;
+    }
+    Node(int data1)
+    {
+        this.data=data1;
+        this.next=null;
+    }
+}
 public class linklst {
-    
+    public static void main(String[] args)
+    {
+        int[] arr={2,3,5,6};
+        Node y=new Node(arr[1]);
+        System.out.println(y.data);
+    }
 }
