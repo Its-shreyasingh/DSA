ded212170a24a5080e4753bfe200eeb4a76522b1
Its-shreyasingh <ssshreya1210@gmail.com>
2025-09-19 01:54:07 +0530

    search-for-value-inLL

--- linklst.java
+++ linklst.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Node
 {
     int data;
@@ -37,10 +38,34 @@ public class linklst {
         }
         return count;
     }
+    private static int checkforval(Node head,int val)
+    {
+        Node temp=head;
+        while(temp !=null)
+        {
+            if(temp.data==val)
+            {
+                return 1;
+                
+            }
+            temp=temp.next;
+        }
+        return 0;
+    }
     public static void main(String[] args)
     {
+        Scanner sc=new Scanner(System.in);
         int[] arr={2,3,5,6,3,5,8};
         Node head=convertarr2ll(arr);
-        System.out.println("Length of the Linklist is:"+lengthofll(head));
+        System.out.println("Enter the value to be searched");
+        int val=sc.nextInt();
+        int result=checkforval(head,val);
+        if(result==1){
+            System.out.println("Value searched in linklist is present");
+        }
+        else{
+            System.out.println("Value searched in linklist is not present");
+        }
+        
     }
 }
